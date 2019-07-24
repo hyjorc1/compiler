ae06ebe3b9e72dc4752291ab9de392fc0f22829a
longvu <longvu@atan-115b-12.cs.iastate.edu>
2019-07-24 14:22:15 -0500

    Add the ability to remove duplication when retrieve json file as well as when restarted in case of power failure

--- src/java/boa/datagen/forges/github/GetReposByLanguage.java
+++ src/java/boa/datagen/forges/github/GetReposByLanguage.java
@@ -1,26 +1,34 @@
 package boa.datagen.forges.github;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.sun.corba.se.impl.orbutil.RepIdDelegator;
+
 import boa.datagen.util.FileIO;
 
-
 public class GetReposByLanguage {
-	
+
 	static int stars = 0;
-	
+
 	public static void main(String[] args) {
 		TokenList tokens = new TokenList(args[0]);
 		String outDir = args[1];
 		String[] languages = args[2].split(",");
-		stars = Integer.parseInt(args[3]);
-		
+		stars = 1;
+
 		if (args.length > 2) {
 			languages = new String[args.length - 2];
 			for (int i = 2; i < args.length; i++)
@@ -39,24 +47,32 @@ public class GetReposByLanguage {
 					e.printStackTrace();
 				}
 	}
-	
-	
+
 	public static class Worker implements Runnable {
-		private final int id; 
+		private final int id;
 		private final String language;
 		private TokenList tokens;
 		private final String outDir;
 		private JsonArray repos = new JsonArray();
 		private final int RECORDS_PER_FILE = 100;
 		private int counter = 0;
-		
+		private HashSet<Integer> processedRepID = new HashSet<>();
+		private ArrayList<Integer> IDtoWrite = new ArrayList<>();
+
 		public Worker(int id, String language, String outDir, TokenList tokenList) {
 			this.id = id;
 			this.language = language;
 			this.outDir = outDir;
 			this.tokens = tokenList;
+			File processedRepos = new File(outDir + "/" + language + "processed.txt");
+			try {
+				processedRepos.createNewFile();
+				Files.lines(processedRepos.toPath()).forEach(repID -> processedRepID.add(Integer.parseInt(repID)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
+
 		@Override
 		public void run() {
 			Calendar cal = Calendar.getInstance();
@@ -65,18 +81,19 @@ public class GetReposByLanguage {
 			int year = cal.get(Calendar.YEAR);
 			int month = cal.get(Calendar.MONTH) + 1; // month starts from 0
 			int day = cal.get(Calendar.DAY_OF_MONTH);
-			
+
 			String monthString = month < 10 ? "0" + month : String.valueOf(month);
-            String dayString = day < 10 ? "0" + day : String.valueOf(day);
-            String time = year + "-" + monthString + "-" + dayString + "T23:59:59Z";
-			
+			String dayString = day < 10 ? "0" + day : String.valueOf(day);
+			String time = year + "-" + monthString + "-" + dayString + "T23:59:59Z";
+
 //			String time = "2018-12-21T01:01:01Z";
 //			String time = year + "-" + month + "-" + day + "T23:59:59Z";
 			Gson parser = new Gson();
-			
-			while (true){
+
+			while (true) {
 				Token tok = this.tokens.getNextAuthenticToken("https://api.github.com/repositories");
-				String url = "https://api.github.com/search/repositories?q=language:" + language +"+stars:>" + stars + "+pushed:<=" + time + "&sort=updated&order=desc&per_page=100";
+				String url = "https://api.github.com/search/repositories?q=language:" + language + "+stars:>=" + stars
+						+ "+pushed:<=" + time + "&sort=updated&order=desc&per_page=100";
 				System.out.println(url);
 				MetadataCacher mc = new MetadataCacher(url, tok.getUserName(), tok.getToken());
 				mc.authenticate();
@@ -92,27 +109,35 @@ public class GetReposByLanguage {
 				}
 				mc.getResponseJson();
 				String content = mc.getContent();
-				
+
 				JsonObject json = null;
 				json = parser.fromJson(content, JsonElement.class).getAsJsonObject();
-		        JsonArray items = json.getAsJsonArray("items");
-		        if (items.size() > 0) {
-			        for (int j = 0; j < items.size(); j++) {
-			        	JsonObject item = items.get(j).getAsJsonObject();
-			        	this.addRepo(item);
-			        	String pushed = item.get("pushed_at").getAsString();
-			        	if (pushed.compareTo(time) < 0){
-			        		time = pushed;
-			        	}
-			        }
-		        }
-		        int count = json.get("total_count").getAsInt();
-		        if (count == items.size())
-		        	break;
+				JsonArray items = json.getAsJsonArray("items");
+				if (items.size() > 0) {
+					for (int j = 0; j < items.size(); j++) {
+						JsonObject item = items.get(j).getAsJsonObject();
+						// check if repository is already saved
+						int repID = item.get("id").getAsInt();
+						if (!processedRepID.contains(repID)) {
+							this.addRepo(item);
+							processedRepID.add(repID);
+							IDtoWrite.add(repID);
+						} else {
+							System.out.println(repID + " already written");
+						}
+						String pushed = item.get("pushed_at").getAsString();
+						if (pushed.compareTo(time) < 0) {
+							time = pushed;
+						}
+					}
+				}
+				int count = json.get("total_count").getAsInt();
+				if (count == items.size())
+					break;
 				if (tok.getNumberOfRemainingLimit() <= 1) {
 					long t = mc.getLimitResetTime() * 1000 - System.currentTimeMillis();
 					if (t >= 0) {
-						System.out.println("Waiting " + (t/1000) + " seconds for sending more requests.");
+						System.out.println("Waiting " + (t / 1000) + " seconds for sending more requests.");
 						try {
 							Thread.sleep(t);
 						} catch (InterruptedException e) {
@@ -122,14 +147,13 @@ public class GetReposByLanguage {
 				}
 			}
 			writeRemainingRepos();
-		} 
-		
+		}
+
 		private void addRepo(JsonObject repo) {
 			File fileToWriteJson = null;
 			this.repos.add(repo);
 			if (this.repos.size() % RECORDS_PER_FILE == 0) {
-				fileToWriteJson = new File(
-						outDir + "/Thread-" + this.id + "-page-" + counter + ".json");
+				fileToWriteJson = new File(outDir + "/Thread-" + this.id + "-page-" + counter + ".json");
 				while (fileToWriteJson.exists()) {
 					System.out.println(fileToWriteJson.getAbsolutePath() + " arleady exist");
 					counter++;
@@ -138,6 +162,17 @@ public class GetReposByLanguage {
 				FileIO.writeFileContents(fileToWriteJson, this.repos.toString());
 				System.out.println(Thread.currentThread().getId() + " " + counter++);
 				this.repos = new JsonArray();
+				try (FileWriter fw = new FileWriter(outDir + "/" + language + "processed.txt", true);
+					BufferedWriter bw = new BufferedWriter(fw);
+					PrintWriter out = new PrintWriter(bw)) {
+					for (Integer repID : IDtoWrite) {
+						out.println(repID.intValue());
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					IDtoWrite.clear();
+				}
 			}
 		}
 
@@ -150,7 +185,7 @@ public class GetReposByLanguage {
 					fileToWriteJson = new File(outDir + "/Thread-" + this.id + "-page-" + counter + ".json");
 				}
 				FileIO.writeFileContents(fileToWriteJson, this.repos.toString());
-				System.out.println(this.id  + counter++);
+				System.out.println(this.id + counter++);
 			}
 		}
 	}
